f39cb0bed492d4d9b051b4cc04c0e2cb68d532f2
gshinohara <65073029+gshinohara@users.noreply.github.com>
2024-02-24 17:57:36 +0900

    プロジェクト作成

---
+++ TellDocEvent/TellDocEvent/TellDocEventPlugin.cs
@@ -0,0 +1,35 @@
+﻿using Rhino;
+using System;
+
+namespace TellDocEvent
+{
+    public class TellDocEventPlugin : Rhino.PlugIns.PlugIn
+    {
+        public TellDocEventPlugin()
+        {
+            Instance = this;
+            RhinoApp.WriteLine($"{this.Name} is loaded.");
+
+            RhinoDoc.AddRhinoObject += RhinoDoc_AddRhinoObject;
+            RhinoDoc.ReplaceRhinoObject += RhinoDoc_ReplaceRhinoObject;
+            RhinoDoc.EndSaveDocument += RhinoDoc_EndSaveDocument;
+        }
+
+        private void RhinoDoc_EndSaveDocument(object sender, DocumentSaveEventArgs e)
+        {
+            RhinoApp.WriteLine("EndSaveDocument");
+        }
+
+        private void RhinoDoc_ReplaceRhinoObject(object sender, Rhino.DocObjects.RhinoReplaceObjectEventArgs e)
+        {
+            RhinoApp.WriteLine("ReplaceRhinoObject");
+        }
+
+        private void RhinoDoc_AddRhinoObject(object sender, Rhino.DocObjects.RhinoObjectEventArgs e)
+        {
+            RhinoApp.WriteLine("AddRhinoObject");
+        }
+
+        public static TellDocEventPlugin Instance { get; private set; }
+    }
+}
